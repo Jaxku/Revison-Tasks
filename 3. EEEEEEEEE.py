57891e1127ed302cf7e7650895ccc99b45b46772
Jaxku <52820208+Jaxku@users.noreply.github.com>
2024-02-08 21:11:30 +1300

    e

---
+++ 3. EEEEEEEEE.py
@@ -0,0 +1,11 @@
+# EEEEEEE
+e = '0'
+
+# checking the stuff you feel me
+sentence = input("write a sentence: ")
+for char in sentence:
+    if char == 'e' or char == 'E':
+        e += '1'
+
+# print
+print("The number of e's in the sentence is:", e)
